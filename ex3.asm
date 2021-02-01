add3c51e74e3cac194423b2af528d6c4601e71c1
billykim618 <billykim618@gmail.com>
2021-01-31 18:33:20 -0800

    update ex3

--- ex3.asm
+++ ex3.asm
@@ -2,9 +2,11 @@ global _start
 
 section .text
 _start:
+	mov ecx, 101	; set ecx to 99
 	mov ebx, 42	; exit status is 42
 	mov eax, 1	; sys_exit system call
-	jmp skip	; jump to "skip" label
+	cmp ecx, 100	; compare ecx to 100
+	jl skip	; jump to "skip" label
 	mov ebx, 13	; exit status is 13
 skip:
 	int 0x80
